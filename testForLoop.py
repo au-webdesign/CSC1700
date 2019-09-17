231a141308f9fdc703178899f29d4f7bf414c6f0
au-webdesign <dlash@aurora.edu>
2019-09-17 16:49:18 -0500

    Inittal Commit

--- testForLoop.py
+++ testForLoop.py
@@ -1,6 +1,13 @@
+# Review Use of Git
 for i in range(2,4):
-    print(f"i:{i}")
+    print(f"i1:{i}")
 print("------------------")
 for i in range(6):
-    print(f"i:{i}")
+    print(f"i2:{i}")
+print("------------------")
+for i in range(5,1,-1):
+    print(f"i3:{i}")
+print("------------------")
+for i in range(2,10,2):
+    print(f"i4:{i}")
 print("------------------")
